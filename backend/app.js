019c9a097bef881ae8f96948f03458f936839322
amirbg2004 <amirbg43@hotmail.com>
2026-04-17 17:17:17 +0300

    feat(backend): establish backend app

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import supabase from './config/database.js';
+
+const app = express();
+
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+
+app.get('/test', async (req, res) => {
+  const { data, error } = await supabase
+    .schema(process.env.SUPABASE_SCHEMA)
+    .from('specialties')
+    .select('specialty_id, name');
+
+  if (error) return res.status(400).json({ error });
+
+  return res.status(200).json({ data });
+});
+
+export default app;
