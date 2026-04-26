73a01deea5bf26eaaad7d2245f40f48bb5fe3068
amirbg2004 <amirbg43@hotmail.com>
2026-04-26 15:29:56 +0300

    feat(backend): integrate "/lawyers" router into the server.

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,13 @@ import express from 'express';
 import cors from 'cors';
 import supabase from './config/database.js';
 
+import lawyerDetailsRouter from './routes/lawyerRoutes.js';
+
 const app = express();
 
 dotenv.config();
 app.use(cors());
+app.use(`/${process.env.API_VERSION}/lawyers`, lawyerDetailsRouter);
 app.use(express.json());
 
 app.get('/test', async (req, res) => {
