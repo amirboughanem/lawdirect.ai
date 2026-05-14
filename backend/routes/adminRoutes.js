e1d78d593cdf40d8a44db2a511d261dc1436e029
amirbg2004 <amirbg43@hotmail.com>
2026-05-14 13:07:14 +0300

    feat(api): expose image upload admin endpoint

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
+import upload from '../middlewares/multer.js';
 import { createLawyer } from '../controllers/lawyerDataController.js';
-// import { insertLawyerEmbedding } from '../controllers/lawyerImageController.js';
+import { uploadLawyerImage } from '../controllers/lawyerImageController.js';
 
 const router = express.Router();
 
 router.post('/lawyers', createLawyer);
-// router.get('/:lawyerId/image', insertLawyerEmbedding);
+router.put('/lawyers/:lawyerId/image', upload.single("image"), uploadLawyerImage);
 
 export default router;
