5faf14c6b9fdb298b0bce6cf4801a206896e7a58
amirbg2004 <amirbg43@hotmail.com>
2026-05-15 15:20:32 +0300

    refactor: delete lawyer image retrieval endpoint and logic

--- backend/routes/lawyerRoutes.js
+++ backend/routes/lawyerRoutes.js
@@ -1,10 +1,8 @@
 import express from 'express';
 import { fetchLawyer } from '../controllers/lawyerDataController.js';
-import { fetchLawyerImage } from '../controllers/lawyerImageController.js';
 
 const router = express.Router();
 
 router.get('/:lawyerId', fetchLawyer);
-router.get('/:lawyerId/image', fetchLawyerImage);
 
 export default router;
